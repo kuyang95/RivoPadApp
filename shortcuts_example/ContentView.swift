e113bf38591afb8cfda280288747bdf65a716170
Pangmo <kuyang95@naver.com>
2026-01-30 11:20:53 +0900

    Initial Commit

---
+++ shortcuts_example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shortcuts_example
+//
+//  Created by meee on 1/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shortcuts_example/shortcuts_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  shortcuts_exampleApp.swift
+//  shortcuts_example
+//
+//  Created by meee on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct shortcuts_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
